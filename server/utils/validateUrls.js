e2fd14344f91aef8d2883ffe658e404059c4ac69
SalmanHabeeb <quadrisyedsalmanhabeeb@gmail.com>
2023-07-25 23:00:02 +0530

    Changed regex validation pattern on server
    Previous url pattern was found to reject urls with '=' sign at specific instances. Ex:- https://colab.research.google.com/drive/1T04II_63yBoUYGJUn-UjmWLuwtrFrl6h#scrollTo=YwMN7RjmQ8PT was rejected

--- server/utils/validateUrls.js
+++ server/utils/validateUrls.js
@@ -1,18 +1,11 @@
 function isValidURL(url2Check) {
-    const pattern = new RegExp(
-        '^([a-zA-Z]+:\\/\\/)?' + // protocol(Optional)
-          '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-          '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR IP (v4) address
-          '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-          '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-          '(\\#[-a-z\\d_]*)?$', // fragment locator
-        'i'
-      );
-    return pattern.test(url2Check);
+  const pattern =
+    /^([a-zA-Z]+:\/\/)?(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+  return pattern.test(url2Check);
 }
 
 function hasHTTPHeader(url2Check) {
-  const headerPattern = new RegExp('^([a-zA-Z]+:\\/\\/)');
+  const headerPattern = new RegExp("^([a-zA-Z]+:\\/\\/)");
   return headerPattern.test(url2Check);
 }
 
@@ -22,10 +15,10 @@ function addHTTPHeader(url) {
 }
 
 function ensureHTTPHeader(url) {
-  return hasHTTPHeader(url)? url:addHTTPHeader(url);
+  return hasHTTPHeader(url) ? url : addHTTPHeader(url);
 }
 
 module.exports = {
   isValidURL,
   ensureHTTPHeader,
-}
+};
